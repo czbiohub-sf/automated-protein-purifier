d7c390f72fe81e3db4e68b8859ee7f6e16c5482f
Robert Puccinelli <robert.puccinelli@outlook.com>
2020-01-31 14:58:11 -0800

    Update device setup script

--- scripts/device_setup.py
+++ scripts/device_setup.py
@@ -3,8 +3,7 @@ import logging
 import socket
 from czpurifier.middleware import DeviceInterface
 
-current_address = socket.gethostbyname(socket.gethostname())
-
+current_address = socket.gethostbyname(socket.getfqdn() + '.local')
 logging.basicConfig(level=logging.INFO)
 di = DeviceInterface(ip_address=current_address)
 di.autorun()
