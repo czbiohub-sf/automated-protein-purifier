c48a1e00f689edd60b7151017749bf42b7665071
Samia Sama <samia.siddique07@gmail.com>
2020-12-08 17:52:48 -0800

    Add docstring to main window and do small bug fix

--- czpurifier/gui/backend/main_window.py
+++ czpurifier/gui/backend/main_window.py
@@ -4,27 +4,42 @@ from custom import BackEnd_CustomWindow
 from calibration import BackEnd_CalibrationWindow
 from czpurifier.gui.control import GUI_Controller
 from czpurifier.gui.frontend import Ui_MainWindow
+from constants_calibration import LOAD_VOLUME_1mL, LOAD_VOLUME_5mL
 import sys
 from time import sleep
 from multiprocessing import set_start_method
+from typing import List
 
 
 class BackEnd_MainWindow(Ui_MainWindow):
+    """Runs the main window
+
+    :param Ui_MainWindow: Frontend display of the main window
+    :type Ui_MainWindow: QMainWindow Class
+    """
+
     def __init__(self, MainWindow):
+        """Display the frontend and initialize the backend
+
+        :param MainWindow: The window displaying the Ui
+        :type MainWindow: QMainWindow
+        """
         self.gui_controller = GUI_Controller()
         super().setupUi(MainWindow)
-        self.initEvents()
+        self.init_events()
 
-    def initEvents(self):
-        """Initialize all buttons"""
-        self.run_calib_prot_btn.clicked.connect(lambda: self.percolumncalib(2))
-        self.purification_btn.clicked.connect(lambda: self.percolumncalib(0))
-        self.otherscripts_btn.clicked.connect(lambda: self.percolumncalib(1))
-        self.close_btn.clicked.connect(self.onClick_close_btn)
-        self.run_sim_btn.clicked.connect(self.onClick_sim_btn)
-        self.columnsize_combo.activated.connect(self.onSelect_columnSize)
+    def init_events(self):
+        """Initialize the backend
+        """
 
-        self.update_calib_disp(self.gui_controller.actualvol1mL, 10)
+        self.run_calib_prot_btn.clicked.connect(lambda: self.on_click_run_protocol(2))
+        self.purification_btn.clicked.connect(lambda: self.on_click_run_protocol(0))
+        self.otherscripts_btn.clicked.connect(lambda: self.on_click_run_protocol(1))
+        self.close_btn.clicked.connect(self.on_click_close)
+        self.run_sim_btn.clicked.connect(self.on_click_simulator)
+        self.columnsize_combo.activated.connect(self.on_select_column_size)
+
+        self.display_calibration_volume(self.gui_controller.actualvol1mL, 10)
         self.columnsize = '1mL'
         self.comboBox.setCurrentIndex(3)
         self.p1_actual.setValidator(QtGui.QDoubleValidator())
@@ -34,10 +49,17 @@ class BackEnd_MainWindow(Ui_MainWindow):
 
         # Timer needed between checking for correct input and opening the new window
         self.start_protocol_timer = QtCore.QTimer()
-        self.start_protocol_timer.timeout.connect(self.startProtocol)
+        self.start_protocol_timer.timeout.connect(self.start_protocol)
 
-    def update_calib_disp(self, actual_val, expected_val):
-        """Update the expected and actual values for the flow volume"""
+    def display_calibration_volume(self, actual_val: List[float], expected_val: List[float]):
+        """Update the display of the expected and actual load volume
+
+        :param actual_val: The actual volume measured after running the pumps
+        :type actual_val: List[float]
+        :param expected_val: The expected volume after running the pumps
+        :type expected_val: List[float]
+        """
+
         self.p1_actual.setText('{}'.format(actual_val[0]))
         self.p2_actual.setText('{}'.format(actual_val[1]))
         self.p3_actual.setText('{}'.format(actual_val[2]))
@@ -48,93 +70,69 @@ class BackEnd_MainWindow(Ui_MainWindow):
         self.p3_expected.setText('{}'.format(expected_val))
         self.p4_expected.setText('{}'.format(expected_val))
 
-    def onSelect_columnSize(self):
-        """Update the default parameter display based on the column size selected"""
+    def on_select_column_size(self):
+        """Update the calibration volume display based on column size
+        """
         if self.columnsize_combo.currentIndex() == 0:
             self.columnsize = '1mL'
             actual_val = self.gui_controller.actualvol1mL
-            expected_val = 10
+            expected_val = LOAD_VOLUME_1mL
         else:
             self.columnsize = '5mL'
             actual_val = self.gui_controller.actualvol5mL
-            expected_val = 25
+            expected_val = LOAD_VOLUME_5mL
         
-        self.update_calib_disp(actual_val, expected_val)
+        self.display_calibration_volume(actual_val, expected_val)
 
-    def onClick_calib_protocol(self):
+    def run_calibration(self):
+        """Opens the calibration window
+        """
+
         num_cols = self.comboBox.currentIndex() + 1
         self.calib = QtWidgets.QMainWindow()
         self.calib_ui = BackEnd_CalibrationWindow(self.calib, self.columnsize, self.percolumn, num_cols)
         self.calib.show()
+    
+    def run_purification(self):
+        """Opens the purification window 
+        """
 
-    def onClick_sim_btn(self):
-        self.confirm_connectSim()
-        if self.is_sure:
-            self.is_sure = None
-            self.run_sim()
-            self.run_sim_btn.setEnabled(False)
-    
-    def confirm_connectSim(self):
-        """Confirms whether or not the user meant to click an action button"""
-        msg = QtWidgets.QMessageBox()
-        msg.setText('Are you sure you want to run the simulator mode?')
-        msg.setInformativeText('If you are connected to the simulator mode and would like to '
-        'switch back to the hardware please close the software and rerun it.\n\n'
-        'Click Ok to start simulator')
-        msg.setIcon(QtWidgets.QMessageBox.Question)
-        msg.setStandardButtons(QtWidgets.QMessageBox.Ok | QtWidgets.QMessageBox.Cancel)
-        msg.buttonClicked.connect(self._msgbtn)
-        msg.exec()
-    
-    def _msgbtn(self, i):
-        """Returns the result from the are you sure pop up"""
-        self.is_sure = True if 'ok' in i.text().lower() else False
-
-    def run_sim(self):
-        """
-        Call the method to run the simulator if 'run simulator' is clicked
-        on the connect to device pop up
-        """
-        self.gui_controller.connect_to_simulator()
-        msg = QtWidgets.QMessageBox()
-        msg.setText('Running Simulator')
-        msg.setStandardButtons(QtWidgets.QMessageBox.Ok)
-        msg.exec()
-    
-    def onClick_purification_btn(self):
-        """
-        Opens the purification window 
-        """
         self.purifier = QtWidgets.QMainWindow()
-        self.purifier_ui = BackEnd_Purification(self.purifier, self.gui_controller.device_process, self.columnsize, self.percolumn)
+        self.purifier_ui = BackEnd_Purification(self.purifier, self.gui_controller.device_process, 
+                                                self.columnsize, self.percolumn)
         self.purifier.show()
 
-    def onClick_otherscripts_btn(self):
+    def run_custom(self):
+        """Opens the custom window
         """
-        Opens the other scripts window
-        """
-        self.oth_sc_window = QtWidgets.QMainWindow()
-        self.oth_sc_ui = BackEnd_CustomWindow(self.oth_sc_window, self.gui_controller.device_process, self.columnsize, self.percolumn)
-        self.oth_sc_window.show() 
-    
-    def onClick_close_btn(self):
-        """
-        Closes the GUI
-        """
-        quit()
+        
+        self.custom_wdw = QtWidgets.QMainWindow()
+        self.custom_ui = BackEnd_CustomWindow(self.custom_wdw, self.gui_controller.device_process, 
+                                            self.columnsize, self.percolumn)
+        self.custom_wdw.show() 
 
-    def percolumncalib(self, is_basic_purification):
-        """Create the list of per column calibration factor based on the input values in the GUI
-        Initiated when either basic purification, column calibration or other script button is clicked"""
+    def on_click_run_protocol(self, protocol_index: int):
+        """Open the selected protocol and pass the calibration factors
+
+        A small timer is started after calculating the calibration factors. The 
+        protocol window is opened once the timer finishes. Timer needed for error
+        pop ups to function properly.
+
+        :param protocol_index: The identifier to know what protocol to run
+        :type protocol_index: int. 0 = basic purification, 1 = custom, 2 = calibration
+        :raises ZeroDivisionError: Pops error if any actual volume is set to 0
+        :raises ValueError: Pops error is any textbox is empty
+        """
+        
         try:
-            expected = 10 if self.columnsize == '1mL' else 25
+            expected = LOAD_VOLUME_1mL if self.columnsize == '1mL' else LOAD_VOLUME_5mL
             self.percolumn = []
             self.percolumn.append(expected/float(self.p1_actual.text()))
             self.percolumn.append(expected/float(self.p2_actual.text()))
             self.percolumn.append(expected/float(self.p3_actual.text()))
             self.percolumn.append(expected/float(self.p4_actual.text()))
             self.start_protocol_timer.start(500)
-            self.is_basic_purification = is_basic_purification
+            self.protocol_index = protocol_index
         except ZeroDivisionError:
             msg = QtWidgets.QMessageBox()
             msg.setText('Error! Require non-zero actual volume for pumps!')
@@ -148,23 +146,62 @@ class BackEnd_MainWindow(Ui_MainWindow):
             msg.setStandardButtons(QtWidgets.QMessageBox.Ok)
             msg.exec()
 
-    def startProtocol(self):
-        """Starts the protocol window after the wait time passes
-        Wait time required for pyqt widgets"""
-        if self.is_basic_purification == 0:
-            self.onClick_purification_btn()
-        elif self.is_basic_purification == 1:
-            self.onClick_otherscripts_btn()
+    def start_protocol(self):
+        """Starts the desired protocol once the timer completes
+        """
+
+        if self.protocol_index == 0:
+            self.run_purification()
+        elif self.protocol_index == 1:
+            self.run_custom()
         else:
-            self.onClick_calib_protocol()
+            self.run_calibration()
         self.start_protocol_timer.stop()
+
+    def on_click_close(self):
+        """Closes the GUI
+        """
+        quit()
+
+    def on_click_simulator(self):
+        """Pops ups are you sure message if simulator mode button is clicked
+        """
+
+        if self.confirm_simulator():
+            self.run_simulator()
+            self.run_sim_btn.setEnabled(False)
+
+    def confirm_simulator(self):
+        """Confirms whether or not the user meant to click an action button
+        """
+
+        msg = QtWidgets.QMessageBox()
+        msg.setText('Are you sure you want to run the simulator mode?')
+        msg.setInformativeText('If you are connected to the simulator mode and would like to '
+        'switch back to the hardware please close the software and rerun it.\n\n'
+        'Click Ok to start simulator')
+        msg.setIcon(QtWidgets.QMessageBox.Question)
+        msg.setStandardButtons(QtWidgets.QMessageBox.Ok | QtWidgets.QMessageBox.Cancel)
+        ret = msg.exec()
+        if ret == QtWidgets.QMessageBox.Ok:
+            return True
+        else:
+            return False
+
+    def run_simulator(self):
+        """Run the simulator to debug the GUI
+        """
+        self.gui_controller.connect_to_simulator()
+        msg = QtWidgets.QMessageBox()
+        msg.setText('Running Simulator')
+        msg.setStandardButtons(QtWidgets.QMessageBox.Ok)
+        msg.exec()
 
 if __name__ == "__main__":
     import sys
-    set_start_method('spawn')
+    set_start_method('spawn')  # Allow for different forked processes to get signals
     app = QtWidgets.QApplication(sys.argv)
-    # Timeout the QEvent to allow for signals to be caught
-    timer = QtCore.QTimer()
+    timer = QtCore.QTimer()  # Timeout the QEvent to allow for signals to be caught
     timer.start(1000)
     timer.timeout.connect(lambda : None)
     MainWindow = QtWidgets.QMainWindow()
